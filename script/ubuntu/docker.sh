ef7e413eb999e20bef58784005a0aa939da34b8c
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-07-23 17:56:22 +0400

    update docker.sh installation script

--- script/ubuntu/docker.sh
+++ script/ubuntu/docker.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+sudo "Deleting old Docker versions if exists..."
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
 echo "Updating system..."
 sudo apt-get update
-sudo apt-get upgrade -y
-
 echo "Installing dependencies..."
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y \
+      apt-transport-https\
+      ca-certificates \
+      curl \
+      gnupg \
+      lsb-release
 
-echo "Adding Docker repository..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+echo "Adding official Docker's GPG key.."
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "Configuring repo..."
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Installing Docker..."
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Starting and enabling Docker service..."
 sudo systemctl start docker
