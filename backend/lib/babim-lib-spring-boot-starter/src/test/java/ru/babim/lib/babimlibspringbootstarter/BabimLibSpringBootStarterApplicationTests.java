782ae65e5c0c54c01d82f775c2db8a13e094a9dd
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-07-31 02:36:40 +0400

    add unresolved files

---
+++ backend/lib/babim-lib-spring-boot-starter/src/test/java/ru/babim/lib/babimlibspringbootstarter/BabimLibSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.babim.lib.babimlibspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabimLibSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
