34732114a6276e24feb037bbbc0f81becb0184f0
babim-negev <captainprice1356@gmail.com>
2024-08-01 23:58:35 +0000

    изменение docker.sh, добавление compose_app.yml

--- script/ubuntu/docker.sh
+++ script/ubuntu/docker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-sudo "Deleting old Docker versions if exists..."
+echo "Deleting old Docker versions if exists..."
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
 echo "Updating system..."
 sudo apt-get update
+
 echo "Installing dependencies..."
 sudo apt-get install -y \
-      apt-transport-https\
+      apt-transport-https \
       ca-certificates \
       curl \
       gnupg \
@@ -24,7 +25,7 @@ echo \
 
 echo "Installing Docker..."
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
 
 echo "Starting and enabling Docker service..."
 sudo systemctl start docker
@@ -33,4 +34,11 @@ sudo systemctl enable docker
 echo "Checking Docker version..."
 docker --version
 
-echo "Docker installation complete!"
+echo "Installing Docker Compose..."
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Checking Docker Compose version..."
+docker-compose --version
+
+echo "Docker and Docker Compose installation complete!"
