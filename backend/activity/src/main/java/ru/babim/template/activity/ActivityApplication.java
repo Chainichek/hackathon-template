56cd7bef6116f29a5a3fcffcbb027690f474177b
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-08-02 18:15:45 +0400

    using discovery registration

--- backend/activity/src/main/java/ru/babim/template/activity/ActivityApplication.java
+++ backend/activity/src/main/java/ru/babim/template/activity/ActivityApplication.java
@@ -2,10 +2,12 @@ package ru.babim.template.activity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ActivityApplication {
 
     public static void main(String[] args) {
