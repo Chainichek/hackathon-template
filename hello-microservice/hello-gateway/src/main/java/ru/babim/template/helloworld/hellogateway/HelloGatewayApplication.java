b2746a87cf1abc8591acf2821c48063b763b3fb2
Chainichek <daniyar-shaymardanov@yandex.ru>
2024-08-08 16:16:03 +0400

    update

--- hello-microservice/hello-gateway/src/main/java/ru/babim/template/helloworld/hellogateway/HelloGatewayApplication.java
+++ hello-microservice/hello-gateway/src/main/java/ru/babim/template/helloworld/hellogateway/HelloGatewayApplication.java
@@ -1,15 +1,17 @@
 package ru.babim.template.helloworld.hellogateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class HelloGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HelloGatewayApplication.class, args);
 	}
-
 }
--- hello-microservice/hello-microservice/src/main/java/ru/babim/template/helloworld/hellomicroservice/config/OpenAPIConfig.java
+++ hello-microservice/hello-microservice/src/main/java/ru/babim/template/helloworld/hellomicroservice/config/OpenAPIConfig.java
@@ -3,11 +3,14 @@ package ru.babim.template.helloworld.hellomicroservice.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition
@@ -28,6 +31,7 @@ public class OpenAPIConfig {
         return new OpenAPI()
                 .info(new Info().title("%s API".formatted(apiAppName))
                         .version(appVersion)
-                        .description(APP_DESCRIPTION.formatted(apiAppName)));
+                        .description(APP_DESCRIPTION.formatted(apiAppName)))
+                .servers(List.of(new Server().url("/%s".formatted(appName)).description("Prod server")));
     }
 }
